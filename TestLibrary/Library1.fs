cb7b1f24c63e3b5b78fadd44a125a749eced2bd3
bonneyab <bonneyab@gmail.com>
2015-02-26 12:46:15 -0800

    Cleanup! And testlibrary
    Added small library I didn't want to lose that works with the
    integration runner

---
+++ TestLibrary/Library1.fs
@@ -0,0 +1,24 @@
+﻿namespace TestLibrary
+open IntegrationServices.Interfaces
+
+type Class1() = 
+  interface IIntegration with
+    member this.Execute args = 
+      //pull data
+      //Export data
+      printfn "I'm executing!"
+    member this.IntegrationName = "Find"
+    member this.Directions = "Magically finds a word, these directions can be updated later"
+
+module Logger =
+  let log text =
+    printfn text
+
+module Searcher = 
+  open Logger
+  let logFind word line fileName = 
+    let test = sprintf "%s found, line %d, file name %s" word line fileName
+    log test
+  let findWordInDirectory directory word =
+    "stuff!"
+
