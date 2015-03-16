dfdd149b0f593e798e62192ca6b995453d02bcf0
bonneyab <bonneyab@gmail.com>
2015-03-16 15:21:08 -0700

    Testing git

--- EulerProblems/FSharpEulerProblems/Library1.fs
+++ EulerProblems/FSharpEulerProblems/Library1.fs
@@ -179,7 +179,7 @@ module Problem8 =
 //a^2 + b^2 = c^2
 //For example, 32 + 42 = 9 + 16 = 25 = 5^2.
 //There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-//Find the product abc.
+//Find the product abc. Test
 module Problem9 =
   let getProductOfPythagoreanTripletForNumber(num : int) =
     let increment triplet =
